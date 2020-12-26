e2ce552d0c9502cedf6bbbe4511bf91a4da9017f
Jose Benitez <pepebenitez12@gmail.com>
2020-12-26 10:04:05 -0500

    Learned how to setup and starta project with vue 3, as well as basic file structure and using components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <modal></modal>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  data() {
+		return {
+			title: 'My first vue 3 app :)'
+		}
+	},
   components: {
-    HelloWorld
+    Modal
   }
 }
 </script>
@@ -22,5 +27,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
